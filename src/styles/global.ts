f7010889147821f0c2940ab4d37126e996b776f6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-12-21 14:31:01 -0300

    style(styles/index.tsx): estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    '-webikit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  },
+})

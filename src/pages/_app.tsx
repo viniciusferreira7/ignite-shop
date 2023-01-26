681011af8098a932bfd45b6b5d1980983671134d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-01-26 10:56:39 -0300

    build(_app.tsx): instalação do use-shopping-cart
    Para integração com o stripe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+
+import { CartProvider } from 'use-shopping-cart'
+
 import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
@@ -8,11 +11,18 @@ globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="Logo do ignite" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider
+      shouldPersist={false}
+      currency="BRL"
+      cartMode="checkout-session"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string}
+    >
+      <Container>
+        <Header>
+          <Image src={logoImg} alt="Logo do ignite" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   )
 }
